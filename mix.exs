99f68433277bbba128af3ee7ac644709d6b2f810
jgourassas <jgourassas@gmail.com>
2022-11-03 02:54:23 +0200

    chest pain data added

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Cardiofields.MixProject do
     [
       app: :cardiofields,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
