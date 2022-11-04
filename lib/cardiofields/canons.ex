753d805f882e42f154ff914cc7abbd54242a524b
jgourassas <jgourassas@gmail.com>
2022-11-04 18:06:25 +0200

    In definitons Show added the options

--- lib/cardiofields/canons.ex
+++ lib/cardiofields/canons.ex
@@ -757,11 +757,13 @@ defmodule Cardiofields.Canons do
 
   ###############
   def all_defs_options(definition_id) do
-    from(
+    query = from(
       dr in Defs_option,
       where: dr.definition_id == ^definition_id,
       order_by: dr.code
     )
+   Repo.all(query)
+
   end
 
   def defs_codes(definition_id) do
